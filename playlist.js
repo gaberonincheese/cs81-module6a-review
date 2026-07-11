b8f0332649418c47e60d3e755f46067bee9b1860
Gabriella Martinez <gabimayern@gmail.com>
2026-07-11 16:17:40 -0700

    Updated playlist.js with comments

--- playlist.js
+++ playlist.js
@@ -1,19 +1,27 @@
 //GitHub link: https://github.com/gaberonincheese/cs81-module6a-review
 
-
+// Constructor function. Uppercase first letter of function to indicate constructor.
+// Takes a name parameter when initializing a new Playlist.
 function Playlist(name) {
+  // "this" refers to the instance of Playlist being created.
   this.name = name;
   this.songs = [];
   this.currentSong = null;
 }
 
+// Accesses the prototype of Playlist to add a method for including a song.
 Playlist.prototype.addSong = function(songTitle) {
-  this.songs.push(songTitle);
+  // Pushing a song to the songs array of the current Playlist instance.
+  this.songs.push(songTitle); // "this" refers to the instance of Playlist being accessed. 
 };
 
+// Accesses the prototype of Playlist to add a method for playing the first song.
 Playlist.prototype.playFirst = function() {
+  // Verifies that the array is more than 0 songs before attempting to play the first song.
   if (this.songs.length > 0) {
+    // The object's currentSong property is set to the first song in the songs array.
     this.currentSong = this.songs[0];
+    // Outputs the current song.
     console.log("Now playing:", this.currentSong);
   }
 };
