2ac13572926b421d5eb405c36a3d1aab7cecb5ba
Gabriella Martinez <gabimayern@gmail.com>
2026-07-11 17:42:50 -0700

    Added more comments
    Also explained meaning of "this" in each method

--- playlist.js
+++ playlist.js
@@ -18,6 +18,7 @@ Playlist.prototype.addSong = function(songTitle) {
 // Accesses the prototype of Playlist to add a method for playing the first song.
 Playlist.prototype.playFirst = function() {
   // Verifies that the array is more than 0 songs before attempting to play the first song.
+  // "this" refers to the instance of Playlist being accessed. 
   if (this.songs.length > 0) {
     // The object's currentSong property is set to the first song in the songs array.
     this.currentSong = this.songs[0];
@@ -26,21 +27,31 @@ Playlist.prototype.playFirst = function() {
   }
 };
 
+// Accesses the prototype of Playlist to add a method for skipping the current song.
 Playlist.prototype.skipSong = function() {
+    // Verifies that the array is more than 0 songs before attempting to skip the current song.
+    // "this" refers to the instance of Playlist being accessed. 
   if (this.songs.length > 1) {
+    // Uses the shift() method to remove the first song from the songs array.
     this.songs.shift();
+    // Sets the currentSong to the new first song in the array.
     this.currentSong = this.songs[0];
+    // Outputs the new current song.
     console.log("Skipped! Now playing:", this.currentSong);
   } else {
+    // If there's no songs in the array, outputs a message indicating that there are no more songs to skip.
     console.log("No more songs to skip.");
   }
 };
 
+// Accesses the prototype of Playlist to add a method for listing all songs.
 Playlist.prototype.listSongs = function() {
+  // Outputs the name of the playlist.
+  // "this" refers to the instance of Playlist being accessed. 
   console.log("Playlist:", this.name);
+  // Outputs the songs in the playlist using the join method to separate songs.
   console.log("Songs:", this.songs.join(", "));
 };
-
 
 let myMix = new Playlist("My Chill Mix");
 myMix.addSong("Lofi Study");
